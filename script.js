e653a1e3123ed33065312030c2b5084153d9b4c8
Caroline Xiong <carolinexiong@Carolines-MacBook-Pro-2.local>
2021-05-01 16:34:53 -0700

    added javascript functionality

--- script.js
+++ script.js
@@ -1,15 +1,143 @@
 // script.js
 
 const img = new Image(); // used to load image from <input> and draw to canvas
+const canvas = document.getElementById('user-image');
+const ctx = canvas.getContext('2d');
+const form = document.querySelector("form");
+const submit = document.querySelector("button[type=submit]");
+const clear = document.querySelector("button[type=reset]");
+const read = document.querySelector("button[type=button]");
+const imgInput = document.querySelector("#image-input");
+const volume = document.getElementById('volume-group');
+const volRange = document.querySelector("input[type=range]");
+
+var top = document.getElementById("text-top");
+var bottom = document.getElementById("text-bottom");
+var voiceSelect = document.getElementById('voice-selection');
+var synth = window.speechSynthesis;
+var voices = [];
+
+function populateVoiceList() {
+  voiceSelect.remove(0);
+  voices = synth.getVoices();
+  voiceSelect.disabled = false;
+
+  for (var i = 0; i < voices.length; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
 
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
-  // TODO
 
+  // toggle buttons
+  submit.disabled = false;
+  clear.disabled = true;
+  read.disabled = true;
+
+  // clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // fill canvas with black
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // get dimensions
+  var dim = getDimmensions(canvas.width,canvas.height,img.width,img.height);
+  ctx.drawImage(img, dim.startX, dim.startY, dim.width, dim.height);
+  
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
   // - Clear the form when a new image is selected
   // - If you draw the image to canvas here, it will update as soon as a new image is selected
+});
+
+imgInput.addEventListener('change', () => {
+  const imgURL = URL.createObjectURL(imgInput.files[0])
+  img.src = imgURL;
+  img.alt = imgInput.files[0].name;
+});
+
+form.addEventListener('submit', function(e) {
+  
+  // prevent reloading
+  e.preventDefault();
+
+  // set text
+  ctx.font = "35px Helvetica";
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.fillText(top.value, canvas.width / 2, 50);
+  ctx.fillText(bottom.value, canvas.width / 2, 375);
+
+  // toggle buttons
+  submit.disabled = true;
+  clear.disabled = false;
+  read.disabled = false;
+});
+
+clear.addEventListener('click', () => {
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // toggle buttons
+  submit.disabled = false;
+  clear.disabled = true;
+  read.disabled = true;
+
+});
+
+read.addEventListener('click', function(e) {
+  
+  // prevent reloading
+  e.preventDefault();
+
+  var utteranceTop = new SpeechSynthesisUtterance(top.value);
+  var utteranceBot = new SpeechSynthesisUtterance(bottom.value);
+  var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+  for (var i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utteranceTop.voice = voices[i];
+      utteranceBot.voice = voices[i];
+    }
+  }
+
+  utteranceTop.volume = volRange.value / 100;
+  utteranceBot.volume = volRange.value / 100;
+  speechSynthesis.speak(utteranceTop);
+  speechSynthesis.speak(utteranceBot);
+});
+
+volume.addEventListener('input', () => {
+
+  const scroll = document.querySelector('#volume-group img');
+
+  if (volRange.value == 0) {
+    scroll.src = 'icons/volume-level-0.svg';
+  }
+  else if (volRange.value <= 33) {
+    scroll.src = 'icons/volume-level-1.svg';
+  }
+  else if (volRange.value <= 66) {
+    scroll.src = 'icons/volume-level-2.svg';
+  }
+  else {
+    scroll.src = 'icons/volume-level-3.svg';
+  }
 });
 
 /**
